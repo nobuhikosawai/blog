ae9848fb057f85e02f45a12d1ba82d8f42e3ebd7
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2021-05-01 00:32:42 +0900

    feat: add a button to toggle dark mode

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'gatsby'
+import { css } from '@emotion/core';
+import { Helmet } from 'react-helmet';
 
 import { rhythm, scale } from '../utils/typography'
 
+const headerLayout = css`
+  display: flex;
+  justify-content: space-between;
+  & > dark-mode-toggle {
+    padding-top: 1.25rem;
+  }
+`;
+
 const Layout = (props) => {
+  // import client-side because it depends on the `document` api.
+  // https://github.com/gatsbyjs/gatsby/issues/16815#issuecomment-527901559
+  useEffect(() => {
+    import('dark-mode-toggle/src/dark-mode-toggle');
+  }, []);
+
   const { location, title, children } = props
   const rootPath = `${__PATH_PREFIX__}/`
   let header
@@ -53,20 +69,40 @@ const Layout = (props) => {
   }
 
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
-      {header}
-      {children}
-      <footer>
-        © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
-    </div>
+    <>
+      <Helmet>
+        <script>{`
+          // If prefers-color-scheme is not supported, fall back to light mode.
+          if (window.matchMedia('(prefers-color-scheme: dark)').media === 'not all') {
+            document.documentElement.style.display = 'none';
+            document.head.insertAdjacentHTML(
+                'beforeend',
+                '<link rel="stylesheet" href="light.css">'
+            );
+          }
+        `}</script>
+        <link rel="stylesheet" href="dark.css" media="(prefers-color-scheme: dark)" />
+        <link rel="stylesheet" href="light.css" media="(prefers-color-scheme: light)" />
+      </Helmet>
+
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <header css={headerLayout}>
+          {header}
+          <dark-mode-toggle />
+        </header>
+        {children}
+        <footer>
+          © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    </>
   );
 }
 
