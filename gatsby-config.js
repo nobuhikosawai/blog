32946ac8dd34938d048c01b9979ce27e7b42df34
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2021-05-04 04:55:37 +0900

    feat: update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/pen-emoji.png`,
       },
     },
     `gatsby-plugin-offline`,
